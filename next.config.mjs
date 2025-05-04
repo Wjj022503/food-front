22de9600178793d348fab6b84d8b549e8c56467d
jingjie <wjingjie0225@gmail.com>
2025-05-04 13:55:29 +0800

    Fix next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'appfood-back-gxd7g3hacpacf7ha.canadacentral-01.azurewebsites.net',
-        pathname: '/images/**',
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'appfood-back-gxd7g3hacpacf7ha.canadacentral-01.azurewebsites.net',
+          pathname: '/images/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
